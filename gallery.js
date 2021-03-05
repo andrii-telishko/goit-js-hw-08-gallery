f306b9d84609e78782068641e455f2b45628864a
Andrii Telishko <telishkoav@gmail.com>
2021-03-05 17:20:17 +0200

    Додав зміну картинок в модальному вікні клавішами

--- gallery.js
+++ gallery.js
@@ -1,13 +1,17 @@
 import galleryItems from './gallery-items.js'
 
-const galleryListRef = document.querySelector('.js-gallery')
-const modalRef = document.querySelector('.js-lightbox')
-const galleryModalIMGRef = document.querySelector('.lightbox__image')
-const modalCloseBtnRef = document.querySelector('.lightbox__button')
-const modalOverlayRef = document.querySelector('.lightbox__overlay')
+const refs = {
+  galleryList: document.querySelector('.js-gallery'),
+  modal: document.querySelector('.js-lightbox'),
+  modalImg: document.querySelector('.lightbox__image'),
+  modalCloseBtn: document.querySelector('.lightbox__button'),
+  modalOverlay: document.querySelector('.lightbox__overlay')
+}
+
+
 
 function createListMarkup(array) {
-    const arrayMarkup =  array.map(({original, preview, description}) => 
+    return array.map(({original, preview, description}) => 
         `
         <li class="gallery__item">
   <a
@@ -23,41 +27,81 @@ function createListMarkup(array) {
   </a>
 </li>
         `
-        )
-    return arrayMarkup;
+        ).join('')
+    
 }
 
-galleryListRef.innerHTML = createListMarkup(galleryItems).join('');
+refs.galleryList.innerHTML = createListMarkup(galleryItems);
 
-galleryListRef.addEventListener('click', onModalOpen)
-modalCloseBtnRef.addEventListener('click', onModalClose)
-modalOverlayRef.addEventListener('click', onModalClose)
-window.addEventListener('keydown', onModalCloseByKeyPress)
+refs.galleryList.addEventListener('click', onModalOpen)
+refs.modalCloseBtn.addEventListener('click', onModalClose)
+refs.modalOverlay.addEventListener('click', onModalClose)
+window.addEventListener('keydown', onModalCloseByKeyDown)
+window.addEventListener('keydown', onModalChangeImgByKeyDown)
+
 
 function onModalOpen(event) {
     event.preventDefault()
-    if (event.target.nodeName !== 'IMG') {
-       return
-    }
-    modalRef.classList.add('is-open')
-  galleryModalIMGRef.src = event.target.dataset.source
-  galleryModalIMGRef.alt = event.target.alt
-  console.log();
+    refs.modal.classList.add('is-open')
+  refs.modalImg.src = event.target.dataset.source
+  refs.modalImg.alt = event.target.alt
+  
 }
 
 function onModalClose(event) {
   if (event.target === event.currentTarget) {
-    modalRef.classList.remove('is-open')
-  galleryModalIMGRef.src = ''
-  galleryModalIMGRef.alt = ''
+    refs.modal.classList.remove('is-open')
+  refs.modalImg.src = ''
+  refs.modalImg.alt = ''
   }
 }
 
-function onModalCloseByKeyPress(event) {
+function onModalCloseByKeyDown(event) {
   
   if (event.code === "Escape") {
-     modalRef.classList.remove('is-open')
+    refs.modal.classList.remove('is-open')
+    refs.modalImg.src = ''
+    refs.modalImg.alt = ''
   }
-  
 }
+
+const arrayWithImgRefs = galleryItems.map(item => item.original)
+const arrayWithAltRefs = galleryItems.map(item => item.description)
+  
+
+function onModalChangeImgByKeyDown(event) {
+    
+    let indexOfImg = arrayWithImgRefs.indexOf(refs.modalImg.src);
+    let indexOfIAlt = arrayWithAltRefs.indexOf(refs.modalImg.alt);
+    const indexOfLastElement = arrayWithImgRefs.length-1
+  if (event.code === 'ArrowRight') {
+    if (indexOfImg < indexOfLastElement) {
+      indexOfImg += 1;
+      indexOfIAlt += 1;
+      refs.modalImg.src = arrayWithImgRefs[indexOfImg];
+      refs.modalImg.alt = arrayWithAltRefs[indexOfImg]
+    }
+    else if (indexOfImg = arrayWithImgRefs[indexOfLastElement]) {
+      refs.modalImg.src = arrayWithImgRefs[0];
+      refs.modalImg.alt = arrayWithAltRefs[0]
+    }
+  }
+  else if (event.code === 'ArrowLeft') {
+    if (indexOfImg > 0) {
+      indexOfImg -= 1;
+      indexOfIAlt -= 1;
+      refs.modalImg.src = arrayWithImgRefs[indexOfImg]
+      refs.modalImg.alt = arrayWithAltRefs[indexOfImg]
+    }
+    else {
+      refs.modalImg.src = arrayWithImgRefs[indexOfLastElement]
+      refs.modalImg.alt = arrayWithAltRefs[indexOfLastElement]
+    }
+    }
+  }
+
+  
+
+  
+  
 
